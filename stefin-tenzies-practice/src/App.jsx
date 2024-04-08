d14921a539a274e1df8645b28ffeccedcc604b88
stefin <stefinracho@gmail.com>
2024-04-08 16:34:00 -0700

    complete tenzies game

--- stefin-tenzies-practice/src/App.jsx
+++ stefin-tenzies-practice/src/App.jsx
@@ -1,5 +1,81 @@
-function App() {
-  return <h1>Stefin Tenzies Practice</h1> 
-}
+import { useEffect, useState } from "react";
+import Die from "./components/Die";
+import { useWindowSize } from "@react-hook/window-size";
+import Confetti from "react-confetti";
 
-export default App
+export default function App() {
+  const { width, height } = useWindowSize();
+  const [tenzies, setTenzies] = useState(false);
+  const [dice, setDice] = useState(allNewDice());
+
+  useEffect(() => {
+    const allHeld = dice.every((die) => die.isHeld);
+    const firstValue = dice[0].value;
+    const allSameValue = dice.every((die) => die.value === firstValue);
+    if (allHeld && allSameValue) {
+      setTenzies(true);
+      console.log("You won!");
+    }
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      id: crypto.randomUUID(),
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    };
+  }
+
+  function allNewDice() {
+    const arr = [];
+    for (let i = 0; i < 10; i++) {
+      arr.push(generateNewDie());
+    }
+    return arr;
+  }
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice((prevDice) =>
+        prevDice.map((die) => {
+          return die.isHeld ? die : generateNewDie();
+        }),
+      );
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+    }
+  }
+
+  function holdDice(id) {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      }),
+    );
+  }
+
+  return (
+    <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">
+        {dice.map((die) => (
+          <Die
+            key={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+          />
+        ))}
+      </div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+      {tenzies && <Confetti width={width} height={height} />}
+    </main>
+  );
+}
---
+++ stefin-tenzies-practice/src/components/Die.jsx
@@ -0,0 +1,11 @@
+export default function Die({ value, isHeld, holdDice }) {
+  const styles = {
+    backgroundColor: isHeld ? "#59E391" : "white",
+  };
+
+  return (
+    <div className="die-face" style={styles} onClick={holdDice}>
+      <h2 className="die-num">{value}</h2>
+    </div>
+  );
+}
